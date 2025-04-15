e43df579c5075044534959271ccd7ea373db8795
ikabeee <carlglz30@gmail.com>
2025-04-15 16:42:19 -0500

    feat: integrate Tailwind CSS and add hero page component
    - Added .postcssrc.json for Tailwind CSS configuration.
    - Updated package.json and package-lock.json to include Tailwind CSS and PostCSS dependencies.
    - Created HeroPageComponent with functionality to change hero name and age.
    - Updated routes to include the new hero page.
    - Enhanced CounterPageComponent to update counter using signals.
    - Added HTML template for HeroPageComponent with buttons for interaction.
    - Updated global styles to include Tailwind CSS.

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html', //Relative path to the HTML file
   styleUrl:'./counter-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush //You can't use zone.js with signals
+  //This means that Angular will not automatically check for changes in this component.
 })
 export class CounterPageComponent{
   counter = 10;
   //Define a signal to track the counter value.
   counterSignal = signal(10);
+  constructor(){
+    setInterval(()=>{
+      this.counter += 1;
+      this.counterSignal.update((prev)=> prev + 1);
+      //This will update the signal value every 2 seconds.
+      console.log('Tick');
+    }, 2000)
+  }
+
 
   increaseBy(value: number){
     this.counter += value;
