3f58d0957e10db703c3cd3831c818a665adda424
Carlos Gonzalez <108252279+ikabeee@users.noreply.github.com>
2025-04-23 21:08:39 -0500

    Delete 03-gifs-app/src/environments directory

--- 03-gifs-app/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-  giphyApiKey: 'WOSHe875OQpwJ7jbU6Elh2KJdh7NADCG',
-  giphyUrl: 'https://api.giphy.com/v1'
-};
