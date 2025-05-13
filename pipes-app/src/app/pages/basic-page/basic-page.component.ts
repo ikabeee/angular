f744f430e0a14d3105eced5b99d527193d70dce2
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-13 08:59:36 -0500

    feat: enhance basic and numbers pages with improved layout and localization support

--- pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+  nameLower = signal('leonardo');
+  nameUpper = signal('Leonardo');
+  fullName = signal('Leonardo da vinci');
 
+
+  customDate = signal(new Date());
+  tickingDataEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+    onCleanUp(() => {
+      clearInterval(interval);
+    })
+  });
 }
--- pipes-app/src/app/pages/numbers-page/numbers-page.component.ts
+++ pipes-app/src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Person } from '../../../../../01-ts-intro/src/topics/08-classes';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.component.html',
   styles: ``
 })
 export default class NumbersPageComponent {
+  totalSells = signal(2_567_789.5567);
+  percent = signal(0.4856);
 
 }
