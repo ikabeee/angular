5ae12da706f591154c7beb3b7c2016c96558d4fc
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-13 11:41:37 -0500

    feat: enhance custom page with hero data display and implement necessary pipes

--- pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/canFly.pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { HeroTextColorPipe } from '../../pipes/heroTextColor.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [toggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe],
   templateUrl: './custom-page.component.html',
   styles: ``
 })
 export default class CustomPageComponent {
   name = signal('Angular');
   upperCase = signal(true);
+  heroes = signal(heroes);
 }
