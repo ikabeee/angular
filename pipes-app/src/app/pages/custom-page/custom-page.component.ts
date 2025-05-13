5c924923abb1f26785b6e0b13e6f8b94f8ebfee1
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-13 11:10:18 -0500

    feat: add hero data and toggle case pipe with custom page integration

--- pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { toggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [toggleCasePipe],
   templateUrl: './custom-page.component.html',
   styles: ``
 })
 export default class CustomPageComponent {
-
+  name = signal('Angular');
+  upperCase = signal(true);
 }
