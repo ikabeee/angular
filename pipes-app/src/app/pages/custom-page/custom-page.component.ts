c81bc538e86562d7b5f4939a3d767608f3b9c491
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-13 12:03:48 -0500

    feat: add sorting functionality for heroes and update custom page with sorting buttons

--- pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -6,10 +6,12 @@ import { HeroColorPipe } from '../../pipes/heroColor.pipe';
 import { HeroTextColorPipe } from '../../pipes/heroTextColor.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
   templateUrl: './custom-page.component.html',
   styles: ``
 })
@@ -17,4 +19,5 @@ export default class CustomPageComponent {
   name = signal('Angular');
   upperCase = signal(true);
   heroes = signal(heroes);
+  sortBy = signal<keyof Hero | null>(null);
 }
