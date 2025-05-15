650b68647482d0f4a733d0ccca9db209bf226561
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-14 22:34:42 -0500

    feat: add environment setup script for Mapbox integration

--- 08-maps/src/app/shared/components/navbar/navbar.component.ts
+++ 08-maps/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
-import { filter, map, tap } from 'rxjs';
+import { filter, map } from 'rxjs';
 
 import { routes } from '../../../app.routes';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -21,16 +21,13 @@ export class NavbarComponent {
     }))
     .filter((route) => route.path !== '**');
 
+  //Observable
   pageTitle$ = this.router.events.pipe(
     filter((event) => event instanceof NavigationEnd),
     // tap((event) => console.log(event)),
     map((event) => event.url),
-    map(
-      (url) =>
-        routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
-    )
-  );
-
+    map((url) => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'));
+  // /Signal
   pageTitle = toSignal(
     this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
