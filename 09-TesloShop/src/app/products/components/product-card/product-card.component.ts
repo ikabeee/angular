d55a61ca638a0d8ef8e4a8e0cd7c48554d3ff44e
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-15 01:59:13 -0500

    feat: add Swiper for product carousel and implement product card component with user interface

--- 09-TesloShop/src/app/products/components/product-card/product-card.component.ts
+++ 09-TesloShop/src/app/products/components/product-card/product-card.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
 @Component({
-  selector: 'app-product-card',
-  imports: [],
+  selector: 'product-card',
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.component.html',
-  styles: ``
 })
 export class ProductCardComponent {
+  product = input.required<Product>();
 
+  imageUrl = computed(() => {
+    return `http://localhost:3000/api/files/product/${
+      this.product().images[0]
+    }`;
+  });
 }
