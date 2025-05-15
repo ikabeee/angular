0bc6dcbab58db558038f06071e6ddaaf65d0560f
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-15 02:03:46 -0500

    feat: implement ProductsService and enhance gender page with product listing and routing

--- 09-TesloShop/src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ 09-TesloShop/src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-front-navbar',
-  imports: [],
+  selector: 'front-navbar',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
-export class FrontNavbarComponent {
-
-}
+export class FrontNavbarComponent {}
