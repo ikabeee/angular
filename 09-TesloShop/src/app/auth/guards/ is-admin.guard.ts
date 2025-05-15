f43bc1a908b63d55d2aa216ed76ac9695f9513e5
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-15 12:22:57 -0500

    fix: correct import order for AuthService in is-admin.guard.ts

--- 09-TesloShop/src/app/auth/guards/ is-admin.guard.ts
+++ 09-TesloShop/src/app/auth/guards/ is-admin.guard.ts
@@ -1,9 +1,10 @@
 
 import { inject } from '@angular/core';
 import { CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 import { firstValueFrom } from 'rxjs';
-import { AuthService } from '@auth/services/auth.service';
+
 
 export const IsAdminGuard: CanMatchFn = async (
   route: Route,
