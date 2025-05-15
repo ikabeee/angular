263a4a357d7d6fd498fcd2c3e24c338641f6b4fe
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-14 20:40:17 -0500

    feat: add mini-map and fullscreen-map components; update houses and markers pages

---
+++ 08-maps/src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  imports: [],
+  templateUrl: './mini-map.component.html',
+  styles: ``
+})
+export class MiniMapComponent {
+
+}
---
+++ 08-maps/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullscreen-map-page',
+  imports: [],
+  templateUrl: './fullscreen-map-page.component.html',
+  styles: ``
+})
+export class FullscreenMapPageComponent {
+
+}
