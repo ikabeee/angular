70262a0c9eecaf5ac70b04fa3877e879285ca9b1
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-15 01:53:28 -0500

    refactor: remove app.component.spec.ts and update app.component.ts
    feat: add app.config.ts with HTTP client support
    
    feat: define routes in app.routes.ts for lazy loading store front
    
    feat: create product-card and product-carrousel components
    
    feat: create products component and its template
    
    feat: create front-navbar component for navigation
    
    feat: implement store-front layout component
    
    feat: add gender-page, home-page, not-found-page, and product-page components
    
    feat: define storeFrontRoutes for the store front module
    
    fix: update index.html title and add data-theme attribute
    
    style: integrate Tailwind CSS and DaisyUI themes in styles.css

---
+++ 09-TesloShop/src/app/products/components/product-carrousel/product-carrousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-carrousel',
+  imports: [],
+  templateUrl: './product-carrousel.component.html',
+  styles: ``
+})
+export class ProductCarrouselComponent {
+
+}
