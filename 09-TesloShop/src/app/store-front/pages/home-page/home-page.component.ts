6ab123028511edd0ea158e07064b711441f409eb
Carlos Gonzalez <carlglz30@gmail.com>
2025-05-15 02:05:33 -0500

    feat: enhance home and product pages with product listing and loading states

--- 09-TesloShop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-TesloShop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
+// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
-  styles: ``
 })
 export class HomePageComponent {
+  productsService = inject(ProductsService);
 
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({});
+    },
+  });
 }
--- 09-TesloShop/src/app/store-front/pages/product-page/product-page.component.ts
+++ 09-TesloShop/src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCarouselComponent } from '@products/components/product-carrousel/product-carrousel.component';
+import { ProductsService } from '@products/services/products.service';
+
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
-  styles: ``
 })
 export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
 
+  productIdSlug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdSlug }),
+    loader: ({ request }) =>
+      this.productService.getProductByIdSlug(request.idSlug),
+  });
 }
